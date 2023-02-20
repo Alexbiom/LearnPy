487593bb7f7618002a424e3f0f68bb1db92f1e3c
Alexey Pononarev <alexbiomashine@gmail.com>
2023-02-20 21:53:58 +0300

    Learn file/line/word/letter

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello Alex!")
+fin = open('words.txt')
+def funk_not_e(fin):
+    count = 1
+    for line in fin:
+        line = fin.readline()
+        word = line.strip()
+        if "e" not in word:
+            if word != "":
+                print("Строка: " + str(count) + " " + word)
+                count += 1
+
+funk_not_e(fin)
+
+
