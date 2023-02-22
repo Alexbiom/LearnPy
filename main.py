a79c580518a41763d349ae211d315f7f5e1cb129
Alexey Pononarev <alexbiomashine@gmail.com>
2023-02-22 17:02:33 +0300

    Add code of binary search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# function of search line/world/letter
+
 fin = open('words.txt')
-def funk_not_e(fin, zer = input("Введите запретную букву: ")):
+def funk_not_e(fin, zer):
     count_up = 1
     count = 1
     for line in fin:
@@ -7,17 +9,34 @@ def funk_not_e(fin, zer = input("Введите запретную букву: "
         word = line.strip()
         if zer not in word:
             if word != "":
-#               print("Строка: " + str(count) + " " + word)
+                #print("Строка: " + str(count) + " " + word)
                 count += 1
         count_up += 1
     proz = count/count_up
     print("Слов всего: " + str(count_up))
-    print("Слов без буквы e: " + str(count))
+    print("Слов без букв(ы) " + zer + " " + str(count))
     print("Процент слов без буквы e: " + str(proz))
-
-funk_not_e(fin)
 
 print("*********END-1*********")
 
+
+# binary search
+
+arr = [i for i in range(1, 101)]
+def binary_search (list, iterm):
+    low = 0
+    high = len(list) - 1
+    while low < high:
+        mid = int((low + high)/2)
+        guess = list[mid]
+        if guess == iterm:
+            return guess
+        if guess > iterm:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+print("*********END-2*********")
 
 
